abd919a1ba17a2d6285d8e867723f2b7a673f22e
hsk980911 <hsk980911@gmail.com>
2022-08-03 17:31:26 +0900

    2442_bj

--- 황순규/1247_bj.py
+++ 황순규/1247_bj.py
@@ -1,7 +1,7 @@
 import sys
 
 for i in range(3):    
-    N = int(sys.stdin.readline())
+    N = int(sys.stdin.readline()) # 시간 초과 발생할 때 input() 대신 사용
     sum = 0
     for i in range(N):
         sum += int(sys.stdin.readline())
