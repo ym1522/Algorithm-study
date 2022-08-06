11cc604f75742833fc752ce1a4fea016ac3d2d3e
HeekunLim <fanghk555@gmail.com>
2022-08-06 10:08:26 +0900

    10808_bj.py
    백준에서는 계속 틀렸다고 나오는데 뭐가 문제일까요???

---
+++ 임희건/10808_bj.py
@@ -0,0 +1,15 @@
+S = input()
+
+count = []
+
+for i in range(26):
+    count.append(0)
+
+# ord(): 아스키코드로 변환
+# 소문자 알파벳 아스키코드 97~122
+for alphabet in S:
+    count[ord(alphabet) - 97] += 1
+
+# 대괄호 없이 출력
+count_str = str(count)[1:-1]
+print(count_str)
