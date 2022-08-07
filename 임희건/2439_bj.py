4bd1f9575d46d76ecfda0a6948d0fe4c2222b359
HeekunLim <fanghk555@gmail.com>
2022-08-07 09:55:13 +0900

    2439_bj.py

---
+++ 임희건/2439_bj.py
@@ -0,0 +1,10 @@
+N = int(input())
+
+for i in range(1, N + 1):
+    for j in range(1, N + 1 - i):
+        print(" ", end = "")
+        
+    for k in range(i):
+        print("*", end = "")
+        
+    print()
