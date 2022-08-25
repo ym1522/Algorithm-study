a615f04db2feda2b607870b583017fef457febcc
soon9 <81350489+hsk980911@users.noreply.github.com>
2022-08-25 21:25:23 +0900

    220825

---
+++ 황순규/스터디/DP/1463_bj.py
@@ -0,0 +1,15 @@
+import sys
+
+X = int(sys.stdin.readline())
+cnt = 0
+
+while X != 1:
+    cnt += 1
+    if X % 3 == 0:
+        X //= 3
+    elif X % 2 == 0:
+        X //= 2
+    else:
+        X -= 1
+
+print(cnt)
